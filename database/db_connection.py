659fbb4d98c642a6f056fdaae2bf384b0f991626
yair <yair999r@gmail.com>
2026-06-17 12:24:33 +0300

    Creating a database connection class

---
+++ database/db_connection.py
@@ -0,0 +1,54 @@
+import mysql.connector
+
+class DB_connection:
+    def __init__(self):
+        self.connct = self.initial_connection()
+
+
+    def initial_connection(self):
+        return mysql.connector.connect(host="localhost", user="root",password='1234' ,port=3307)
+    
+    def create_database(self):
+        cursor = self.connct.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS intelligence_db")
+        cursor.execute("USE intelligence_db")
+        cursor.close()
+
+
+    def create_tabels(self):
+        cursor = self.connct.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS agents(
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       name VARCHAR(30) NOT NULL,
+                       specialty VARCHAR(50) NOT NULL,
+                       is_active BOOLEAN DEFAULT TRUE,
+                       completed_mission INT DEFAULT 0,
+                       failed_mission INT DEFAULT 0,
+                       agent ENUM ('Junior', 'Senior', 'Commander'))""")
+        self.connct.commit()
+
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS missions(
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                       title VARCHAR(30) NOT NULL,
+                       description TEXT NOT NULL,
+                       location VARCHAR(50) NOT NULL,
+                       difficulty INT NOT NULL,
+                       importance INT NOT NULL,
+                       status VARCHAR(30) DEFAULT 'NEW',
+                       risk_level VARCHAR(100) DEFAULT NULL,
+                       assigned_agent_it INT DEFAULT NULL
+)""")
+        self.connct.commit()
+        cursor.close()
+
+    def get_connection(self):
+        if not self.connct.is_connected():
+            self.connct = self.initial_connection()
+        return self.connct
+        
+
+if __name__ == "__main__":
+    db = DB_connection()
+    db.create_database()
+    db.create_tabels()
